3d7ea12fbd3e02b742061376e0ba3c805ea6d327
taylus <brandon@legacy.codes>
2019-07-22 21:58:26 -0500

    Add D-pad input

--- gbdk/02-hello-sprites/README.md
+++ gbdk/02-hello-sprites/README.md
@@ -1,5 +1,5 @@
 # Hello Sprites
-A Game Boy ROM which displays a small animated smiley face using GBDK.
+A Game Boy ROM which displays a small animated smiley face using GBDK. Also handles input from the D-pad.
 
 Compile by running `make` from this directory.
 
@@ -8,3 +8,4 @@ Compile by running `make` from this directory.
 # Resources
 * [Game Boy Tiler Designer](http://www.devrs.com/gb/hmgd/gbtd.html)
 * https://www.youtube.com/watch?v=rCN-jwYn7Qw
+* https://www.youtube.com/watch?v=VYtLiKJY_gc
--- gbdk/02-hello-sprites/src/main.c
+++ gbdk/02-hello-sprites/src/main.c
@@ -14,14 +14,44 @@ void init()
     SHOW_SPRITES;
 }
 
+void input()
+{
+    const int movement_speed = 6;
+
+    int buttons = joypad();
+
+    if (buttons & J_LEFT)
+    {
+        scroll_sprite(0, -movement_speed, 0);
+    }
+    else if (buttons & J_RIGHT)
+    {
+        scroll_sprite(0, movement_speed, 0);
+    }
+
+    if (buttons & J_UP)
+    {
+        scroll_sprite(0, 0, -movement_speed);
+    }
+    else if (buttons & J_DOWN)
+    {
+        scroll_sprite(0, 0, movement_speed);
+    }
+}
+
 void main()
 {
     int curr_sprite_tile = 0;
+    
     init();
+    
     while(1)
     {
+        input();
+        
         curr_sprite_tile ^= 1;
         set_sprite_tile(0, curr_sprite_tile);
+        
         delay(500);
     }
 }
